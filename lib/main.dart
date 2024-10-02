0a141c5e29b301141ee4676e192d673d47deb690
Panagiotis Mantas <mantas.pan1@gmail.com>
2024-10-02 13:55:31 +0300

    add multiple custom particles

--- lib/main.dart
+++ lib/main.dart
@@ -24,70 +24,43 @@ class _NewtonExampleAppState extends State<NewtonExampleApp> {
 
   @override
   Widget build(BuildContext context) {
-    const primaryColor = <int, Color>{
-      50: Color.fromRGBO(27, 27, 29, .1),
-      100: Color.fromRGBO(27, 27, 29, .2),
-      200: Color.fromRGBO(27, 27, 29, .3),
-      300: Color.fromRGBO(27, 27, 29, .4),
-      400: Color.fromRGBO(27, 27, 29, .5),
-      500: Color.fromRGBO(27, 27, 29, .6),
-      600: Color.fromRGBO(27, 27, 29, .7),
-      700: Color.fromRGBO(27, 27, 29, .8),
-      800: Color.fromRGBO(27, 27, 29, .9),
-      900: Color.fromRGBO(27, 27, 29, 1),
-    };
-
-    final width = MediaQuery
-        .of(context)
-        .size
-        .width;
+    final width = MediaQuery.of(context).size.width;
     return MaterialApp(
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: const MaterialColor(
-          0x1b1b1d,
-          primaryColor,
-        ),
-        canvasColor: const Color(0xff1b1b1d),
-      ),
-      home: Center(
-        child: Newton(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xff3A3A3A),
+        body:
+        Newton(
           key: _newtonKey,
           effectConfigurations: [
-          // Emulate light balls falling
-            RelativisticEffectConfiguration(
+            // Emulate light balls falling
+            for  (var i = 0; i < _imageAssets.length; i++)
+              RelativisticEffectConfiguration(
               gravity: Gravity.zero,
-          particleCount: 20,
-          particlesPerEmit: 1,
-              // origin: Offset.zero,
+              particleCount: 1,
+              maxVelocity: Velocity.snail,
+              emitCurve: Curves.decelerate,
+              particlesPerEmit: 1,
               origin: const Offset(0.5, 0.01),
-              // maxOriginOffset: const Offset(1, 0),
-          // distanceCurve: Curves.slowMiddle,
-          // emitCurve: Curves.fastOutSlowIn,
-          // fadeInCurve: Curves.easeIn,
-          // fadeOutCurve: Curves.easeOut,
-          emitDuration: const Duration(milliseconds: 250),
-          // minAngle: 90,
-          maxAngle: 90,
-          // minDistance: 90,
-          // maxDistance: 220,
-          maxParticleLifespan: const Duration(hours: 3),
-          minFadeOutThreshold: 0.6,
-          maxFadeOutThreshold: 0.8,
-          minBeginScale: 0.7,
-          maxBeginScale: 0.9,
-          minEndScale: 1,
-          maxEndScale: 1.2,
-          particleConfiguration: ParticleConfiguration(
-            shape: _imageAssets[0],
-            size: Size.square(50),
-          ),
-          startDelay: Duration.zero,
-          // origin: const Offset(0, 1),
-        ),
-        ],
+              emitDuration: const Duration(seconds: 2),
+              maxAngle: 90,
+              maxParticleLifespan: const Duration(hours: 3),
+              minFadeOutThreshold: 0.6,
+              maxFadeOutThreshold: 0.8,
+              minBeginScale: 0.7,
+              maxBeginScale: 0.9,
+              minEndScale: 1,
+              maxEndScale: 1.2,
+              particleConfiguration: ParticleConfiguration(
+                shape: _imageAssets[i],
+                size: Size(width / 5, width / 5),
+              ),
+                startDelay: Duration(milliseconds: i * 5000),
+              // origin: const Offset(0, 1),
             ),
-      ),);
+          ],
+        ),
+      ),
+    );
   }
 }
