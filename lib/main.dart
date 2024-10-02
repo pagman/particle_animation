03c45a083b8854e3a44b9045d9ea0a76a469c930
Panagiotis Mantas <mantas.pan1@gmail.com>
2024-10-02 13:15:03 +0300

    custom icom for particle

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart' hide Velocity;
 import 'package:newton_particles/newton_particles.dart';
 
@@ -13,43 +15,79 @@ class NewtonExampleApp extends StatefulWidget {
 }
 
 class _NewtonExampleAppState extends State<NewtonExampleApp> {
+  final _newtonKey = GlobalKey<NewtonState>();
+  final List<ImageAssetShape> _imageAssets = [
+    ImageAssetShape('images/bubble.png'),
+    ImageAssetShape('images/thumb_up_1.png'),
+    ImageAssetShape('images/thumb_up_1.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    const primaryColor = <int, Color>{
+      50: Color.fromRGBO(27, 27, 29, .1),
+      100: Color.fromRGBO(27, 27, 29, .2),
+      200: Color.fromRGBO(27, 27, 29, .3),
+      300: Color.fromRGBO(27, 27, 29, .4),
+      400: Color.fromRGBO(27, 27, 29, .5),
+      500: Color.fromRGBO(27, 27, 29, .6),
+      600: Color.fromRGBO(27, 27, 29, .7),
+      700: Color.fromRGBO(27, 27, 29, .8),
+      800: Color.fromRGBO(27, 27, 29, .9),
+      900: Color.fromRGBO(27, 27, 29, 1),
+    };
+
+    final width = MediaQuery
+        .of(context)
+        .size
+        .width;
     return MaterialApp(
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black, brightness: Brightness.dark),
+        primaryColor: const MaterialColor(
+          0x1b1b1d,
+          primaryColor,
+        ),
         canvasColor: const Color(0xff1b1b1d),
       ),
-      home: Newton(
-        effectConfigurations: [
+      home: Center(
+        child: Newton(
+          key: _newtonKey,
+          effectConfigurations: [
           // Emulate light balls falling
-          RelativisticEffectConfiguration(
-            gravity: Gravity.zero,
-            origin: Offset.zero,
-            maxOriginOffset: const Offset(1, 0),
-            maxAngle: 90,
-            maxEndScale: 1,
-            emitDuration: const Duration(seconds: 20),
-            maxVelocity: Velocity.custom(1.05),
-            minVelocity: Velocity.custom(1),
-            maxFadeOutThreshold: 1,
-            maxParticleLifespan: const Duration(days: 1),
-            minAngle: 90,
-            minEndScale: 1,
-            minFadeOutThreshold: 1,
-            particleCount: 80,
-            particlesPerEmit: 1,
-            minParticleLifespan: const Duration(days: 1),
-            particleConfiguration:  ParticleConfiguration(
-              shape: CircleShape(),
-              size:  Size( width/10, width/10),
-            ),
+            RelativisticEffectConfiguration(
+              gravity: Gravity.zero,
+          particleCount: 20,
+          particlesPerEmit: 1,
+              // origin: Offset.zero,
+              origin: const Offset(0.5, 0.01),
+              // maxOriginOffset: const Offset(1, 0),
+          // distanceCurve: Curves.slowMiddle,
+          // emitCurve: Curves.fastOutSlowIn,
+          // fadeInCurve: Curves.easeIn,
+          // fadeOutCurve: Curves.easeOut,
+          emitDuration: const Duration(milliseconds: 250),
+          // minAngle: 90,
+          maxAngle: 90,
+          // minDistance: 90,
+          // maxDistance: 220,
+          maxParticleLifespan: const Duration(hours: 3),
+          minFadeOutThreshold: 0.6,
+          maxFadeOutThreshold: 0.8,
+          minBeginScale: 0.7,
+          maxBeginScale: 0.9,
+          minEndScale: 1,
+          maxEndScale: 1.2,
+          particleConfiguration: ParticleConfiguration(
+            shape: _imageAssets[0],
+            size: Size.square(50),
           ),
+          startDelay: Duration.zero,
+          // origin: const Offset(0, 1),
+        ),
         ],
-      ),
-    );
+            ),
+      ),);
   }
 }
